c84b6ad4b990daf25c35242ad9519c75ff432f59
dharnil mody <dharnil.armour@outlook.com>
2024-01-14 23:22:54 +1100

    ContentSection.vue: Project Tag Added

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import About from './About.vue'
+import Project from './Project.vue'
 </script>
 
 <template>
     <div id="content-section">
+        <Project />
         <About />
     </div>
 </template>
