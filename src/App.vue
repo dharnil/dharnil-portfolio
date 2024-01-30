2d983bca6afb42b9d2520c30291750982d355e67
dharnil mody <dharnil.armour@outlook.com>
2024-01-30 22:56:40 +1100

    App.vue: Test Boiler Plate Code Added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
-<script lang="ts">
+<script setup lang="ts">
 import SideBar from './components/SideBar.vue'
 import Hero from './components/Hero.vue'
+import Project from './components/Project.vue'
+import About from './components/About.vue'
 </script>
 
 <template>
